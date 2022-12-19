d0bb25564e6372e67d83be67243689e2e5c8e64e
ellenfel <ellfelfen@gmail.com>
2022-12-19 18:37:19 +0300

    Finish step 1 and 2 of algo

--- filter-design.py
+++ filter-design.py
@@ -1,5 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+#impors libs
+import numpy 
+from PIL import Image
+
+I = Image.open("cameraman-og.png").convert("L")
+I.show()
+    
+Inoisy = Image.open("salty-cameraman.jpg").convert("L")
+Inoisy.show()
+
+Inoisy_arr = numpy.array(Inoisy)
+h=len(Inoisy_arr)
+w=len(Inoisy_arr[0])
+B = numpy.ones((h,w))
+S = numpy.zeros((h,w))
+
 #proposed filter application
+#Step 1: determining the noisy pixels in the matrix
+
+for i in range(h):
+    for j in range(w):
+        if(Inoisy_arr[i,j] == 0 or Inoisy_arr[i,j] == 255):
+            B[i,j] = Inoisy_arr[i,j];
+        
+#Step 2: Set the binary mask(to detect the position)
+for i in range(h):
+    for j in range(w):
+        if(B[i,j] != 1):
+            S[i,j] = 1
+        else:
+            S[i,j] = 0
+        
+Inoisy_before = Inoisy
+
+k = 3;
+fkh = k//2
+fkw = k//2
+
+U = numpy.pad(Inoisy_arr, (1, 1))
+Y = numpy.pad(S, (1, 1))
+m= 0
+n= 0
+
 
