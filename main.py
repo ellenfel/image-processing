82a2d2cb2619bbb51ca8d29fefc48c3a5669474f
ellenfel <ellfelfen@gmail.com>
2023-01-21 13:27:58 +0300

    add a todo

--- main.py
+++ main.py
@@ -17,6 +17,13 @@ removed_noise_mf = filters.median_filter(arr, 3)
 
 # results
 img1 = Image.fromarray(removed_noise_pd)
-img1.show()
+img1.show() # our method
 img2 = Image.fromarray(removed_noise_mf)
-img2.show()
+img2.show() # median filter
+
+#todo:
+    #add header file
+    #add generating noise option
+    #continue working on purpossed method
+    #find a name for so called "purpossed method"
+    #figure out how to perform assessment on results
