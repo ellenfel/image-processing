1dc1db05883677bf0f891225586f63b7c5688fd6
ellenfel <ellfelfen@gmail.com>
2023-01-21 14:05:59 +0300

    add noise generation class to main.py

--- get_salty.py
+++ get_salty.py
@@ -5,14 +5,14 @@ from PIL import Image
 class generatorXnoise:
     def __init__(self, generate, overwrite):
         self.generate = generate
-        self.ocerwrite = overwrite
+        self.overwrite = overwrite
         
     def generate(img):
         # Getting the dimensions of the image
     	row , col = img.shape
     	
-        #randomly putting 10000 salt
-    	number_of_pixels = 150000
+        #randomly putting 170000 salt
+    	number_of_pixels = 190000
     	for i in range(number_of_pixels):
     	
     		# Pick a random y coordinate
@@ -25,8 +25,8 @@ class generatorXnoise:
     		img[y_coord][x_coord] = 255
     		
 
-        #randomly putting 10000 salt
-    	number_of_pixels = 150000
+        #randomly putting 170000 salt
+    	number_of_pixels = 190000
     	for i in range(number_of_pixels):
     	
     		# Pick a random y coordinate
@@ -39,19 +39,17 @@ class generatorXnoise:
     		img[y_coord][x_coord] = 0
     		
     	return img
+    
+    def overwrite():
 
-# salt-and-pepper noise can
-# be applied only to grayscale images
-# Reading the color image in grayscale image
-img = cv2.imread('cameraman-og.png',
-				cv2.IMREAD_GRAYSCALE)
+        # salt-and-pepper noise can
+        # be applied only to grayscale images
+        # Reading the color image in grayscale image
+        img = cv2.imread('cameraman-og.png',
+        				cv2.IMREAD_GRAYSCALE)
 
-#Storing the image
-cv2.imwrite('salty-cameraman.jpg', 
-            generatorXnoise.generate(img))
+        #Storing the image
+        cv2.imwrite('salty-cameraman.jpg', 
+                    generatorXnoise.generate(img))
 
-def main():
-    img = Image.open("salty-cameraman.jpg")
-    img.show()
-
-main()
+        return img
